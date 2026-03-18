afca94faf27c203a54cd891fd2a397c14efdfc7c
Bayarmaa <b.bayarmaa0321@gmail.com>
2026-03-18 12:46:14 +0530

    Fix backend test infrastructure - resolve port conflicts and app import issues

--- backend/src/tests/auth.test.js
+++ backend/src/tests/auth.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../server');
+const { app } = require('../server');
 const { query } = require('../db/connection');
 const redisClient = require('../cache/redis');
 
